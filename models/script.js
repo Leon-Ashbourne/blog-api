0d2e56341d5d010e857680c8e9e87655c1b497f1
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2026-02-04 14:58:29 +0530

    create middleware functions and the route to get users. Mock users data for now.

--- models/script.js
+++ models/script.js
@@ -1,11 +1,30 @@
 const prisma = require('./prisma');
 
+//mock data
+const users = {
+    1: {
+        username: "priest",
+        password: "123456",
+    },
+    2: {
+        username: "old man",
+        password: "baldman",
+    },
+};
+
 async function createUser(username, password) {
     //mock create user
     console.log(username, password);
     return;
 } 
 
+async function getUsers() {
+    //mock user data
+    const data = users;
+    return { data };
+}
+
 module.exports = {
     createUser,
+    getUsers,
 }
