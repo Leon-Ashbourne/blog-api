ffcbc1fe3e79f1a218c05ebba44848c9e5b7670f
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2026-02-06 10:19:25 +0530

    create a route and middlwares to delete a post

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,5 +1,5 @@
 const Router = require("express").Router;
-const { requestPostsGet, postByIdGet, postsByUserIdGet } = require("../controllers/postsController");
+const { requestPostsGet, postByIdGet, postsByUserIdGet, postDelete } = require("../controllers/postsController");
 
 const postRouter = Router();
 
@@ -8,7 +8,8 @@ postRouter.get('/', requestPostsGet )
 // postRouter.post('/', )
 
 postRouter.get('/:postId', postByIdGet);
-postRouter.get('/d4ef65asdf1e88rtt512eo/:userId', postsByUserIdGet )
+postRouter.delete('/:postId', postDelete )
+postRouter.get('/d4ef65asdf1e88rtt512eo/:userId', postsByUserIdGet );
 
 postRouter.post('/', (req, res) => {
     res.json({
