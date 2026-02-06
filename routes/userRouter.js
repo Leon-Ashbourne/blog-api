f48e5c508d94f7ed653666a00b0372e6c2f3da73
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2026-02-06 10:40:48 +0530

    remove comments

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,18 +10,4 @@ userRouter.put('/:userId', updateUserPost);
 userRouter.get('/:userId', usersByIdGet);
 userRouter.delete('/:userId', userDelete);
 
-//<------ these routes don't make sense ------->//
-
-// userRouter.put('/', (req, res) => {
-//     res.json({
-//         message: "users update request"
-//     });
-// });
-
-// userRouter.delete('/', (req, res) => {
-//     res.json({
-//         message: "users delete request"
-//     });
-// });
-
 module.exports = userRouter;
