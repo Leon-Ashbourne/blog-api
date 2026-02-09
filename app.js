edb8f801fdc8b71b14a629d72e1c99a877c31f00
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2026-02-09 16:42:18 +0530

    adding acccess to allowed origins

--- app.js
+++ app.js
@@ -5,6 +5,21 @@ const passport = require('./authentication/passport');
 const app = express();
 app.use(express.json());
 
+const allowedOrigins = ["localhost:5173"]
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
+
+    if(allowedOrigins.includes(origin)) res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+    if(req.method === 'OPTIONS') {
+        return res.status(200).end();
+    }
+
+    next();
+})
+
 //routes
 app.use('/posts', postRouter);
 app.use('/users', userRouter);
