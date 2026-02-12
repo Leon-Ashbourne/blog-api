90a6ff4c895dfe2df9935ec1bc8b56dc263e0daa
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2026-02-12 17:39:38 +0530

    update logic by adding encrytpion to the password.

--- authentication/jwt.js
+++ authentication/jwt.js
@@ -1,8 +1,9 @@
 const { matchedData, body } = require('express-validator');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 require('dotenv/config');
 
-const { getUserLogin } = require("../models/script");
+const { getPasswordByUsername } = require("../models/script");
 const { handleValidationErrors } = require('../controllers/errors/errorControllers');
 
 //validate and sanitize
@@ -15,9 +16,10 @@ const validate = [
 ]
 
 async function verifyUser(req, res, next) {
+
     const { username, password } = matchedData(req);
-    
-    const { data, error }  = await getUserLogin(username, password);
+
+    const { data, error } = await getPasswordByUsername(username);
 
     if(error) {
         return res.status(503).json({
@@ -25,22 +27,34 @@ async function verifyUser(req, res, next) {
         });
     };
 
-    res.locals.data = data;
-    console.log(data);
+    const foundUser = await bcrypt.compare(password, data.password);
+    if(!foundUser) {
+        return res.status(406).json({
+            message: "username or password is incorrect."
+        })
+    }
+    const { id } = data;
+
+    res.locals.data = {
+        id,
+        username
+    };
     next();
 }
 
 function generateToken(req, res) {
     const payload = res.locals.data;
+    const username = payload.username;
 
-    jwt.sign(payload, process.env.SECRET_KEY, (error, token) => {
+    jwt.sign(payload, process.env.SECRET_KEY, {expiresIn: 7*24*60*60*1000 }, (error, token) => {
         if(error) {
             return res.json({
                 error,
             });
         }
         return res.json({
-            Token: token
+            Token: token,
+            username
         });
     });
 }
