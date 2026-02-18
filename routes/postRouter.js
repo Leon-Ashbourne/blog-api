94af32a4b00eeb6de168901f1fcce106145c6bd6
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2026-02-18 23:29:38 +0530

    protect post route.

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,16 +1,18 @@
 const Router = require("express").Router;
 const passport = require("../authentication/passport");
-const { requestPostsGet, postByIdGet, postsByUserIdGet, postDelete, postUpdate, postCreate } = require("../controllers/postsController");
+const { requestPostsGet, postsGet, postsByUserIdGet, postDelete, postUpdate, postCreate } = require("../controllers/postsController");
 
 const postRouter = Router();
 
 //post routes
-postRouter.get('/', requestPostsGet )
-postRouter.post('/', passport.authenticate('jwt', { session: false, failureMessage: "authentication failed" }) ,postCreate)
+// postRouter.get('/', requestPostsGet )
+const failureMessage = "authentication failed"
 
-postRouter.get('/:postId', postByIdGet);
+postRouter.post('/', passport.authenticate('jwt', { session: false, failureMessage }) ,postCreate)
+
+postRouter.get('/', passport.authenticate('jwt', { session: false, failureMessage }), postsGet);
 postRouter.delete('/:postId', postDelete);
 postRouter.put('/:postId', postUpdate);
-postRouter.get('/d4ef65asdf1e88rtt512eo/:userId', postsByUserIdGet);
+// postRouter.get('/d4ef65asdf1e88rtt512eo/:userId', postsByUserIdGet);
 
 module.exports = postRouter;
