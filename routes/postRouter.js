f8c9c5fe0741673ad34adc7ed6d0326a691e67aa
Leon Ashbourne <leonmichaelasbourne@gmail.com>
2026-02-25 14:59:23 +0530

    add routes as RESTful.

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,18 +1,20 @@
 const Router = require("express").Router;
 const passport = require("../authentication/passport");
-const { requestPostsGet, postsGet, postsByUserIdGet, postDelete, postUpdate, postCreate } = require("../controllers/postsController");
+const { publicPostsGet, postsGet, postsByUserIdGet, postDelete, postUpdate, postCreate, updatePublishPut, updatePostPut } = require("../controllers/postsController");
 
 const postRouter = Router();
 
 //post routes
-// postRouter.get('/', requestPostsGet )
 const failureMessage = "authentication failed"
 
-postRouter.post('/', passport.authenticate('jwt', { session: false, failureMessage }) ,postCreate)
+postRouter.get('/search', publicPostsGet)
+postRouter.post('/', passport.authenticate('jwt', { session: false, failureMessage }) ,postCreate);
 
 postRouter.get('/', passport.authenticate('jwt', { session: false, failureMessage }), postsGet);
 postRouter.delete('/:postId', postDelete);
 postRouter.put('/:postId', postUpdate);
-// postRouter.get('/d4ef65asdf1e88rtt512eo/:userId', postsByUserIdGet);
+
+postRouter.put('/publish/:postId', updatePublishPut);
+postRouter.put('/update/:postid', updatePostPut)
 
 module.exports = postRouter;
